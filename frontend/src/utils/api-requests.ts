eb89dc2e30372c82a93ef256bdc49b2809fe4f8f
Mishe4ka14 <mishechka.14@yandex.ru>
2024-07-16 03:50:53 +0300

    change url

--- frontend/src/utils/api-requests.ts
+++ frontend/src/utils/api-requests.ts
@@ -2,7 +2,7 @@ import {
   IUser, IRegisterResponse, ISearchUser, ILoginInfo, IUpdateUserInfo, IChatOwners, IChatAndUserResponse,
 } from '../services/types/types';
 
-export const API_URL: string = 'http://api.hedwig.nomoredomainswork.ru/';
+export const API_URL: string = 'https://api.hedwig.nomoredomainswork.ru/';
 export const WSS_URL: string = 'ws://api.hedwig.nomoredomainswork.ru/';
 
 const checkResponse = <T>(res: Response): Promise<T> => {
